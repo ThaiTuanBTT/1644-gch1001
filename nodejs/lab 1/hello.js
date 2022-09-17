27ff05fb00ccd90ecb7b5dd575671f51d4726df0
ThaiTuanBTT <tuanbtgch200717@fpt.edu.vn>
2022-09-17 16:06:23 +0700

    ok

--- nodejs/lab 1/hello.js
+++ nodejs/lab 1/hello.js
@@ -1,1 +1,6 @@
 console.log('Hello world')
+console.log("University of Greenwich (Vietnam)")
+console.log(2022)
+console.log('Today is Saturday. \n Tomorrow is Sunday')
+
+console.log("Name: Tuan \nJob: IT student")
