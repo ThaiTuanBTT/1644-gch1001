97d647a74399360b6bbacde9c7f3b8ab8b734186
ThaiTuanBTT <tuanbtgch200717@fpt.edu.vn>
2022-09-17 16:42:53 +0700

    node

--- nodejs/lab 1/hello.js
+++ nodejs/lab 1/hello.js
@@ -1,6 +1,10 @@
+// Câu lệnh NodeJS có thể kết thúc bằng dấu ; hoặc không cần
+//để in ra text(String) thì có thể dùng dấu nháy đơn hoặc nháy kép
 console.log('Hello world')
 console.log("University of Greenwich (Vietnam)")
+    //Để in ra số thì không cần dấu nháy
 console.log(2022)
+    // \n (new line) dùng để xuống dòng
 console.log('Today is Saturday. \n Tomorrow is Sunday')
 
 console.log("Name: Tuan \nJob: IT student")
---
+++ nodejs/lab 1/variable.js
@@ -0,0 +1,18 @@
+// C1: Khai báo biến bằng "var"
+var a = 3
+var b = 5
+var c = a + b
+var d = a * b
+console.log("a = " + a)
+console.log("b = " + b)
+console.log("c = a + b" + c)
+console.log("d = a * b" + d)
+
+// C2: Khai báo biến số(có thể thay đổi được) bằng "let"
+let city = "Hanoi"
+let country = "Vietnam"
+console.log(city + " is the capital of " + country)
+
+//C3:Khai báo hằng số(không thể thay đổi được) bằng "const"
+const pi = 3.14
+console.log("Pi = " + pi)
